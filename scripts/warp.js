6c7298cd7dfd8fdba241244997915ce9ba33fd0d
Evan Warner <evan.k.warner@gmail.com>
2024-12-06 10:29:36 -0500

    revamp 1 commit

--- scripts/warp.js
+++ scripts/warp.js
@@ -1,23 +1,24 @@
-const canvas = document.getElementById('warp'), c = canvas.getContext('2d'), LINE_MAX = 7;
-let cw, ch, initial = true, q, arrStars = [], arrTextStars = [];
+const canvas = document.getElementById('warp'), ctx = canvas.getContext('2d'), LINE_MAX = 7;
+let cnvsWidth, cnvsHeight, firstRender = true, quantStars, arrStars = [], arrTextStars = [], gaussMean = cnvsHeight/2;
 window.onresize = reset;
-function Star(x, y, v, w, s, fx = null, t = false) {
-    this.x = x; this.y = y
-    this.v = v; this.w = w
-    this.s = s; this.fx = fx
+
+function Star(valXPos, valYPos, valVelocity, valDiameter, valRGB, finalX = null, t = false) {
+    this.x = valXPos; this.y = valYPos
+    this.v = valVelocity; this.w = valDiameter
+    this.s = valRGB; this.fx = finalX
     this.t = t
     this.draw = () => {
-        c.beginPath()
-        c.fillStyle = this.s
-        c.strokeStyle = this.s
+        ctx.beginPath()
+        ctx.fillStyle = this.s
+        ctx.strokeStyle = this.s
         if (!this.t) {
-            c.lineWidth = this.w
-            c.moveTo(this.x, this.y)
-            c.lineTo(this.x + v, this.y)
-            c.stroke()
+            ctx.lineWidth = this.w
+            ctx.moveTo(this.x, this.y)
+            ctx.lineTo(this.x + valVelocity, this.y)
+            ctx.stroke()
         }
-        c.arc(this.x, this.y, this.w / 2, 0, Math.PI * 2, true)
-        c.fill()
+        ctx.arc(this.x, this.y, this.w / 2, 0, Math.PI * 2, true)
+        ctx.fill()
     }
     this.update = () => {
         if (this.fx === null) { this.x -= this.v }
@@ -26,59 +27,78 @@ function Star(x, y, v, w, s, fx = null, t = false) {
         this.draw()
     }
 }
+
 function init() {
     document.title = 'Evan Warner'
     resizeHandler(); starBuilder(); animate(); setTimeout(() => getPixelPoints('Evan Warner', 3, '#ffffff'), 800);
     // setTimeout(() => getPixelPoints('javascript developer', 1.8, 'rgba(255,0,0,.7)'), 2000)
-    initial = false
+    firstRender = false
 }
+
 function reset() {
     arrStars = []; arrTextStars = [];
-    initial = true
+    firstRender = true
     resizeHandler(); starBuilder(); getPixelPoints()
-    initial = false
+    firstRender = false
 }
-function starBuilder(count = q) {
+
+//generate parameters for stars
+function starBuilder(count = quantStars) {
     for (let i = 0; i < count; i++) {
-        let x = initial ? Math.floor(Math.random() * cw) + 1 : cw,
-            y = Math.random() * ch + 1,
-            d = calcDist(1, 600, 2.5),
-            rd = Math.floor(Math.min((255 / d) * 300, 255)),
-            g = Math.floor(Math.min((255 / d) * 90, 255)),
-            b = Math.floor(Math.min((255 / d) * 50, 255)),
-            v = Math.min(13 / d, 3),
-            w = Math.max(Math.min((LINE_MAX / d) * 7, LINE_MAX), .4),
-            s = `rgba(${rd},${g},${b},1)`;
-        y = Math.min(y - w, ch);
-        arrStars.push(new Star(x, y, v, w, s))
+        let xPos = firstRender ? Math.floor(Math.random() * cnvsWidth) + 1 : cnvsWidth,
+            yPos = randomGaussian(cnvsHeight/2,380),
+            zPos = calcDist(1, 600, 2.5),
+            rgbRed = 255,
+            rgbGreen = Math.floor(Math.min((255 / (zPos*Math.random())*70), 210)),
+            rgbBlue = Math.floor(Math.min((255 / (zPos*Math.random())*35), 165)),
+            velocity = Math.min(15 / zPos, 3),
+            diameter = Math.max(Math.min((LINE_MAX / (zPos/2)) * 7, LINE_MAX), .4),
+            rgbFinal = `rgba(${rgbRed},${rgbGreen},${rgbBlue},1)`;
+        yPos = Math.min(yPos - diameter, cnvsHeight);
+        arrStars.push(new Star(xPos, yPos, velocity, diameter, rgbFinal))
     }
 }
+
 function letterParticleBuilder(color) {
     for (i = 0; i < arrTextStars.length; i++) {
-        let x = cw,
+        let x = cnvsWidth,
             y = arrTextStars[i].y,
-            v = (cw / 2 / 60) - (Math.random() * 5) + 3,
-            w = Math.max(Math.floor(cw * .002), 1),
+            v = (cnvsWidth / 2 / 60) - (Math.random() * 5) + 3,
+            w = Math.max(Math.floor(cnvsWidth * .0015), 1),
             s = color,
             fx = arrTextStars[i].x;
         arrStars.push(new Star(x, y, v, w, s, fx, true))
     }
     arrTextStars = []
 }
+
+//reevaluate canvas dimensions on resize
 function resizeHandler() {
-    cw = canvas.width = window.innerWidth
-    ch = canvas.height = window.innerHeight
-    q = Math.floor((cw + ch) / 5)
+    cnvsWidth = canvas.width = window.innerWidth
+    cnvsHeight = canvas.height = window.innerHeight
+    quantStars = 900
 }
+
 function getPixelPoints(keyword, hDiv, color) {
+    //establish temporary canvas, used to break text into pixel data
     let temp = document.createElement('canvas'), tempC = temp.getContext('2d');
-    temp.width = cw; temp.height = ch
-    let gridX = Math.floor(cw * .003) + 1, gridY = gridX, fs = Math.floor(cw * .15);
+    temp.width = cnvsWidth; temp.height = cnvsHeight
+
+    //define sample density for pixels from text in temporary canvas, smaller x increases density (cnvsWidth * x)
+    let gridX = Math.floor(cnvsWidth * .003) + 1, gridY = gridX, fs = Math.floor(cnvsWidth * .06);
+    
+    //set parameters with which to draw text from 'keyword'
     tempC.fillStyle = 'red'; tempC.font = `${fs}px Lato sans-serif`
     tempC.fillText(keyword, temp.width / 2 - tempC.measureText(keyword).width / 2, temp.height / hDiv)
+    
+    //evaluate image and define pixel positions
     let idata = tempC.getImageData(0, 0, temp.width, temp.height)
     let buffer32 = new Uint32Array(idata.data.buffer)
-    if (arrTextStars.length > 0) arrTextStars = []
+    
+    //clear arrTextStars if data is already present
+    arrTextStars = []
+
+    //
     for (let y = 0; y < temp.height; y += gridY) {
         for (let x = 0; x < temp.width; x += gridX) {
             if (buffer32[y * temp.width + x]) {
@@ -90,11 +110,15 @@ function getPixelPoints(keyword, hDiv, color) {
     letterParticleBuilder(color)
 }
 function animate() {
-    requestAnimationFrame(animate)
-    c.fillStyle = 'rgba(0,0,0,.18)'
-    c.fillRect(0, 0, cw, ch)
+
+    //clear previous frame
+    ctx.clearRect(0,0,ctx.width,ctx.height)
+    //set background color
+    ctx.fillStyle = "black"
+    ctx.fillRect(0, 0, cnvsWidth, cnvsHeight)
     arrStars.sort(function (a, b) { return a.w - b.w; });
     arrStars.forEach((star, index) => {
+        ctx.globalAlpha = 1 / (star.v/12)
         let pos = star.x -= star.v, cont = true
         if (pos <= 0) {
             arrStars.splice(index, 1)
@@ -105,6 +129,7 @@ function animate() {
             star.update()
         }
     })
+    requestAnimationFrame(animate)
 }
 function calcDist(min, max, skew) {
     var u = 0, v = 0;
@@ -119,4 +144,13 @@ function calcDist(min, max, skew) {
     num += min; // offset to min
     return num;
 }
+
+function randomGaussian(mean = 0, stdDev = 3) {
+    let u = 0, v = 0;
+    while (u === 0) u = Math.random(); //Converting [0,1] to (0,1)
+    while (v === 0) v = Math.random();
+    let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+    return num * stdDev + mean;
+  }
+
 window.onload = init()
